fad8570265affdfab765444b1ef5f5ba117e8a96
Zachary Ebenfeld <zackebenfeld@gmail.com>
2025-10-31 02:20:24 -0400

    swag, comments, openapi, i think not what i want

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
+// @title go-service-template API
+// @version 0.1.0
+// @description Minimal API for books
+// @host localhost:8080
+// @BasePath /
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
 )
 
 type Book struct {
@@ -17,15 +22,24 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("go-service-template, https://github.com/softwarebyze/go-service-template"))
 	})
-	http.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		books := []Book{
-			{"1", "To Kill a Mockingbird", "Harper Lee"},
-			{"2", "Lord of the Flies", "William Golding"},
-		}
-		json.NewEncoder(w).Encode(books)
-
-	})
+	// BooksHandler serves a typed list of books.
+	// @Summary List books
+	// @Description Get a list of books
+	// @Tags books
+	// @Produce json
+	// @Success 200 {array} Book
+	// @Router /books [get]
+	http.HandleFunc("/books", BooksHandler)
 	fmt.Println("listening on :8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+// BooksHandler returns a typed JSON array of Book
+func BooksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	books := []Book{
+		{ID: "1", Title: "To Kill a Mockingbird", Author: "Harper Lee"},
+		{ID: "2", Title: "Lord of the Flies", Author: "William Golding"},
+	}
+	json.NewEncoder(w).Encode(books)
 }
