1c0dd639d023d7ef433e567a261587de63c4f37a
Zachary Ebenfeld <zackebenfeld@gmail.com>
2025-10-31 01:32:07 -0400

    type the response

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
+
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
 
 func main() {
 	fmt.Println("running go-service-template")
@@ -12,10 +19,12 @@ func main() {
 	})
 	http.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(`[
-			{"id": "1", "title": "To Kill a Mockingbird", "author": "Harper Lee"},
-			{"id": "2", "title": "Lord of the Flies", "author": "William Golding"}
-		]`))
+		books := []Book{
+			{"1", "To Kill a Mockingbird", "Harper Lee"},
+			{"2", "Lord of the Flies", "William Golding"},
+		}
+		json.NewEncoder(w).Encode(books)
+
 	})
 	fmt.Println("listening on :8080")
 	http.ListenAndServe(":8080", nil)
