e2714bedae0532183f6798bf723a07233844ab4b
Zachary Ebenfeld <zackebenfeld@gmail.com>
2025-10-31 01:25:01 -0400

    add books endpoint with json response

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("go-service-template, https://github.com/softwarebyze/go-service-template"))
 	})
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
+	http.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`[
+			{"id": "1", "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+			{"id": "2", "title": "Lord of the Flies", "author": "William Golding"}
+		]`))
 	})
 	fmt.Println("listening on :8080")
 	http.ListenAndServe(":8080", nil)
